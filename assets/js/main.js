0bd079cd58061eee857aeef00a697588093ca121
federossi93 <rossifederico0993@gmail.com>
2022-10-12 16:32:10 +0200

    add array and loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,55 @@
-console.log('ciao');
+
+/*Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro:
+avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci 
+dell'array fornito e un semplice ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.*/
+
+
+
+//carosello
+
+const carosello = [
+    "./assets/img/01.webp",
+    "./assets/img/02.webp",
+    "./assets/img/03.webp",
+    "./assets/img/04.webp",
+    "./assets/img/05.webp",
+];
+
+//console.log(carosello);
+
+console.log(carosello);
+
+//seleziono elemeto dom dove applicare le immagini
+
+const caroselloEl = document.querySelector('.box');
+
+//creo una variabile per monitorare la slide attiva
+
+
+let immagineAttiva = 0
+
+
+//creo un loop dentro l'array
+
+for (let i = 0; i < carosello.length; i++) {
+    const slideUrl = carosello[i];
+
+    const slideMarkup = `<img class="${i === immagineAttiva ? 'active' : ''}" src="${slideUrl}" alt="">` 
+    caroselloEl.insertAdjacentHTML('beforeend', slideMarkup)
+    
+    console.log(slideMarkup);
+}
+
+
+
+
+/*MILESTONE 3
+Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. */
