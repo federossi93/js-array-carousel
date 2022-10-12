0382e89c88f95184677cdb1c51b525739ebe772e
federossi93 <rossifederico0993@gmail.com>
2022-10-12 17:58:05 +0200

    add bnt event

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,6 @@ const carosello = [
 
 //console.log(carosello);
 
-console.log(carosello);
 
 //seleziono elemeto dom dove applicare le immagini
 
@@ -34,22 +33,53 @@ const caroselloEl = document.querySelector('.box');
 //creo una variabile per monitorare la slide attiva
 
 
-let immagineAttiva = 0
+let immagineAttiva = 0;
 
 
 //creo un loop dentro l'array
 
 for (let i = 0; i < carosello.length; i++) {
+    //Scorro nel mio array carosello e mi salvo ogni elemento del carosello all interno di slideUrl
     const slideUrl = carosello[i];
+    // se i è = 0 allora metti la classe active altrimenti se non è = 0 non inserisci la classe 
+    const slideMarkup = `<img class="${i === immagineAttiva ? 'active' : ''}" src="${slideUrl}" alt="">`;
+    // inserisco prima della fine di box la costante con la mia immagine al suo interno (slideUrl = SlideMarkup)
+    caroselloEl.insertAdjacentHTML('beforeend', slideMarkup);
 
-    const slideMarkup = `<img class="${i === immagineAttiva ? 'active' : ''}" src="${slideUrl}" alt="">` 
-    caroselloEl.insertAdjacentHTML('beforeend', slideMarkup)
-    
     console.log(slideMarkup);
 }
 
 
-
-
 /*MILESTONE 3
 Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. */
+
+//seleziono bottoni
+
+const btnNext =document.querySelector('.next');
+
+const btnback =document.querySelector('.back');
+
+
+//evento click btnNext
+
+btnNext.addEventListener('click',function() {
+    console.log('next');
+
+    //seleziona tutte le immagini nel markup con lo scopo di trovare quella attiva
+    const slides = document.querySelectorAll('box > img');
+
+    //console.log(carosello[immagineAttiva]);  //visualizzo l'immagine attualmente attiva
+    const currentSlide = carosello[immagineAttiva] 
+
+    //tolgo la classe attiva
+    currentSlide.classList.remove('active')
+
+})
+
+
+//evento click btnback
+
+btnback.addEventListener('click',function() {
+    console.log('back');
+})
+
